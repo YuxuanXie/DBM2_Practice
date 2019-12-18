e0419c2f543ba49cb1c91985c30a4f049172ecc7
David Pojunas <davidapojunas@gmail.com>
2019-12-18 10:22:12 +0100

    Yx:

--- clustering/clustering.py
+++ clustering/clustering.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.cluster import MiniBatchKMeans
+from sklearn.cluster import MiniBatchKMeans, DBSCAN
 from matplotlib import pyplot as plt
 
 
@@ -11,22 +11,29 @@ for t in {"taken_", "upload_"}:
   df["date_"+t[0:-1]] = df["date_"+t[0:-1]].cat.codes
   df = df.drop(columns = ["date_" + t + key for key in time.keys()])
 
-df = df.drop(columns=["user", "tags", "title", "date_taken"])
+df = df.drop(columns=["id", "user", "tags", "title", "date_taken", "date_upload"])
+df  = df.drop_duplicates(subset=None, keep='first', inplace=False)
 
-# df = df.head(100000)
+#normalize the data, for time 
+#remove doubles
 
-kmeans = MiniBatchKMeans(batch_size=100000, max_iter=1000000).fit(df)
-
+kmeans = MiniBatchKMeans(batch_size=100000, max_iter=1000000, n_clusters=12).fit(df)
+# kmeans = DBSCAN(eps=3,min_samples=2).fit(df);
+BBox = ((df["lat"].min(),   df["lat"].max(), df["long"].min(), df["long"].max()))
 
 df["category"] = kmeans.predict(df)
 
 groups = df.groupby(["category"])
 
+image = plt.imread('map_lyon.png')
+
 for name, group in groups:
-  plt.scatter(group["long"],group["lat"] )
+  plt.scatter(group["lat"],group["long"], s=1)
 
 # for index, row in df.iterrows() :
 #   plt.scatter(row["lat"], row["long"], c=row["category"])
+
+plt.imshow(image, zorder=0, extent = BBox, aspect= 'equal')
 
 plt.show()
 
